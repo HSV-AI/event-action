c3a7ccc135453c39e2e5c2540c15272d14719f0a
J. Langley <jameselangley@gmail>
2021-05-16 05:42:18 +0000

    Working on passing token as an argument.

--- script.py
+++ script.py
@@ -1,3 +1,12 @@
+from github import Github
 import os
+import re
+import requests
+import yaml
+import sys
 
+# token = os.getenv("repo-token")
+# g = Github(token)
+print(sys.argv)
+print('Environment')
 print(os.environ)
