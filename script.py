ba0862c56952061642c53788dea6aa8f25c1f394
J. Langley <jameselangley@gmail>
2021-05-16 07:03:53 +0000

    Updated to only trigger on scheduled label.

--- script.py
+++ script.py
@@ -6,9 +6,6 @@ import yaml
 import sys
 import json
 
-print('Environment')
-print(os.environ)
-
 token = os.getenv("INPUT_REPO_TOKEN")
 repo_string = os.getenv("GITHUB_REPOSITORY")
 g = Github(token)
@@ -17,46 +14,37 @@ repo = g.get_repo(repo_string)
 
 path = os.getenv("GITHUB_EVENT_PATH")
 
-
 with open(path) as f:
   data = json.load(f)
 
-print(data)
+print(data['issue']['number'])
+issue_number = data['issue']['number']
 
+issue = repo.get_issue(number=issue_number)
+print(issue.title)
+print(issue.body)
 
-# issue = repo.get_issue(number=12)
-# print(issue.title)
-# print(issue.body)
+# Build regular expression to parse images
+p = re.compile(r'^\!\[[^\]]*]\(([^\)]*)\)')
+image_url = None
 
-# is_presentaiton = False
-# print("\nISSUE LABELS")
-# labels = issue.get_labels()
-# for label in labels:
-#     print(label.name)
-#     if label.name == 'presentation':
-#         is_presentation = True
+# Let's look at all the comments
+print("\nISSUE COMMENTS:")
+comments = issue.get_comments()
+for comment in comments:
+    results = p.search(comment.body)
+    if results:
+        image_url = results.group(1)
+        print(image_url)
+    else:
+        dict = yaml.safe_load(comment.body)
+        print(dict)
 
-# # Build regular expression to parse images
-# p = re.compile(r'^\!\[[^\]]*]\(([^\)]*)\)')
-# image_url = None
-
-# # Let's look at all the comments
-# print("\nISSUE COMMENTS:")
-# comments = issue.get_comments()
-# for comment in comments:
-#     results = p.search(comment.body)
-#     if results:
-#         image_url = results.group(1)
-#         print(image_url)
-#     else:
-#         dict = yaml.safe_load(comment.body)
-#         print(dict)
-
-# # Download the image (if found)
-# if image_url:
-#     filename = 'temp.jpg'
-#     request = requests.get(image_url, stream=True)
-#     if request.status_code == 200:
-#         with open(filename, 'wb') as image:
-#             for chunk in request:
-#                 image.write(chunk)
+# Download the image (if found)
+if image_url:
+    filename = 'temp.jpg'
+    request = requests.get(image_url, stream=True)
+    if request.status_code == 200:
+        with open(filename, 'wb') as image:
+            for chunk in request:
+                image.write(chunk)
