01789847f70a933ef306a78bf2324a4480fe159d
J. Langley <jameselangley@gmail>
2021-05-16 06:04:23 +0000

    Changed to trigger on issue

--- script.py
+++ script.py
@@ -5,8 +5,47 @@ import requests
 import yaml
 import sys
 
-token = os.getenv("INPUT_REPO_TOKEN")
-g = Github(token)
-
 print('Environment')
 print(os.environ)
+
+token = os.getenv("INPUT_REPO_TOKEN")
+repo_string = os.getenv("GITHUB_REPOSITORY")
+g = Github(token)
+
+repo = g.get_repo(repo_string)
+# issue = repo.get_issue(number=12)
+# print(issue.title)
+# print(issue.body)
+
+# is_presentaiton = False
+# print("\nISSUE LABELS")
+# labels = issue.get_labels()
+# for label in labels:
+#     print(label.name)
+#     if label.name == 'presentation':
+#         is_presentation = True
+
+# # Build regular expression to parse images
+# p = re.compile(r'^\!\[[^\]]*]\(([^\)]*)\)')
+# image_url = None
+
+# # Let's look at all the comments
+# print("\nISSUE COMMENTS:")
+# comments = issue.get_comments()
+# for comment in comments:
+#     results = p.search(comment.body)
+#     if results:
+#         image_url = results.group(1)
+#         print(image_url)
+#     else:
+#         dict = yaml.safe_load(comment.body)
+#         print(dict)
+
+# # Download the image (if found)
+# if image_url:
+#     filename = 'temp.jpg'
+#     request = requests.get(image_url, stream=True)
+#     if request.status_code == 200:
+#         with open(filename, 'wb') as image:
+#             for chunk in request:
+#                 image.write(chunk)
